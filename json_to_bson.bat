365e85d26afc81d9ec9c7b5b28fbc616ccbba1f6
MatthewJA <matthew.alger@gmail.com>
2016-03-02 12:38:33 +1100

    JSON -> BSON

---
+++ json_to_bson.bat
@@ -0,0 +1,3 @@
+call mongoimport --db radio --collection radio_classifications data/radio_classifications.json
+call mongoimport --db radio --collection radio_groups data/radio_groups.json
+call mongoimport --db radio --collection radio_subjects data/radio_subjects.json
