a970125801a5fb212f81a6d7da19d09e4685ef69
MatthewJA <matthew.alger@gmail.com>
2016-03-08 10:53:47 +1100

    Fixes duplicate key errors #8.

--- mongo_load_json.bat
+++ mongo_load_json.bat
@@ -1,3 +1,3 @@
-call mongoimport --db radio --collection radio_classifications data/radio_classifications.json
-call mongoimport --db radio --collection radio_groups data/radio_groups.json
-call mongoimport --db radio --collection radio_subjects data/radio_subjects.json
+call mongoimport --db radio --collection radio_classifications data/radio_classifications.json --drop
+call mongoimport --db radio --collection radio_groups data/radio_groups.json --drop
+call mongoimport --db radio --collection radio_subjects data/radio_subjects.json --drop
--- mongo_load_json.sh
+++ mongo_load_json.sh
@@ -1,3 +1,3 @@
-mongoimport --db radio --collection radio_classifications data/radio_classifications.json
-mongoimport --db radio --collection radio_groups data/radio_groups.json
-mongoimport --db radio --collection radio_subjects data/radio_subjects.json
+mongoimport --db radio --collection radio_classifications data/radio_classifications.json --drop
+mongoimport --db radio --collection radio_groups data/radio_groups.json --drop
+mongoimport --db radio --collection radio_subjects data/radio_subjects.json --drop
