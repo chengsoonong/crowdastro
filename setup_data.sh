d37a247cbd6c4cc3558fb9aec93980fb23c4e617
MatthewJA <matthew.alger@gmail.com>
2016-04-06 18:35:59 +1000

    Fixed data setup script.

--- setup_data.sh
+++ setup_data.sh
@@ -1,5 +1,7 @@
 mkdir data
 tar -zxvf sanitized_radio_2016-03-01.tar.gz -C data
+mv data/sanitized_radio_2016-03-01/* data/
+rmdir data/sanitized_radio_2016-03-01
 gunzip -c ATLASDR3_cmpcat_23July2015.dat.gz > data/ATLASDR3_cmpcat_23July2015.dat
 mkdir data/elais
 tar -zxvf elais_11JAN2014_2x2_5x5.tgz -C data/elais
