9bdc7cb06e42c6dfd95c0e7ed14e9fc50dd97212
MatthewJA <matthew.alger@gmail.com>
2016-05-26 15:29:53 +1000

    Added information about SWIRE catalogue. #58

--- setup_data.sh
+++ setup_data.sh
@@ -3,6 +3,7 @@ tar -zxvf sanitized_radio_2016-03-01.tar.gz -C data
 mv data/sanitized_radio_2016-03-01/* data/
 rmdir data/sanitized_radio_2016-03-01
 gunzip -c ATLASDR3_cmpcat_23July2015.dat.gz > data/ATLASDR3_cmpcat_23July2015.dat
+gunzip -c SWIRE3_CDFS_cat_IRAC24_21Dec05.tbl.gz > data/SWIRE3_CDFS_cat_IRAC24_21Dec05.tbl
 mkdir data/elais
 tar -zxvf elais_11JAN2014_2x2_5x5.tgz -C data/elais
 mkdir data/cdfs
