f1d287e90ab4ff55dacc8ea35f75168046925fea
MatthewJA <matthew.alger@gmail.com>
2016-08-04 17:39:19 +1000

    Updated passive crowd example. #122

--- examples/passive_crowd_test.py
+++ examples/passive_crowd_test.py
@@ -4,65 +4,77 @@ import sys
 
 import matplotlib.pyplot as plt
 import numpy
+import sklearn.cluster
 import sklearn.datasets
 
 sys.path.insert(1, os.path.join('..', 'crowdastro', 'active_learning'))
 import passive_crowd
 
-lr_init = False
-skip_zeros = False
+logging.captureWarnings(True)
 
-n_annotators, n_dim, n_samples = 20, 2, 50
+# Generate some data.
+n_annotators, n_dim, n_samples = 4, 2, 50
 x, z = sklearn.datasets.make_classification(
         n_samples=n_samples,
         n_features=n_dim,
         n_informative=n_dim,
         n_redundant=0,
-        random_state=100)
-z = z.reshape((-1, 1))
-y = z.repeat(n_annotators, axis=1).T
-if True:
-    # Annotator 1 is good, but only if y[0] > 0.
-    y[0, y[:, 0] <= 0] = numpy.round(
-            numpy.random.uniform(size=((y[:, 0] <= 0).sum(),)))
-    # Annotator 2 is wrong 10% of the time.
-    indices = numpy.arange(n_samples)
-    numpy.random.shuffle(indices)
-    indices = indices[:n_samples // 10]
-    indices.sort()
-    y[1, indices] = 1 - y[1, indices]
-    # Annotator 3 is good, but only if x[0] > 0.
-    y[2, x[:, 0] <= 0] = numpy.round(
-            numpy.random.uniform(size=((x[:, 0] <= 0).sum(),)))
-    # ALl other annotators are wrong 50% of the time.
-    for annotator in range(3, n_annotators):
-        indices = numpy.arange(n_samples)
-        numpy.random.shuffle(indices)
-        indices = indices[:n_samples * 3 // 10]
-        indices.sort()
-        y[annotator, indices] = 1 - y[annotator, indices]
-assert y.shape == (n_annotators, n_samples)
+        flip_y=0.1,
+        class_sep=2.0,
+        random_state=10)
+z = z.reshape((-1, 1)).astype(bool)
 
-logging.root.setLevel(logging.DEBUG)
-logging.captureWarnings(True)
-a, b, w, g = passive_crowd.train(x, y, lr_init=lr_init, skip_zeros=False)
-print('w:', w)
-print('g:', g)
+# Generate annotator labels. Cluster the data using k-means, then let each
+# annotator be an expert on one cluster. Switch labels at random 35% of the time
+# for all other clusters.
+y = numpy.zeros((n_annotators, n_samples), dtype=bool)
+k_means = sklearn.cluster.KMeans(n_clusters=n_annotators) 
+clusters = k_means.fit_predict(x)
+for annotator in range(n_annotators):
+    for sample in range(n_samples):
+        if clusters[sample] == annotator or numpy.random.random() > 0.35:
+            y[annotator, sample] = z[sample]
+        else:
+            # Flip labels at random for non-expert regions.
+            y[annotator, sample] = not z[sample]
+
+# Train and predict using the passive crowd algorithm.
+a, b, w, g = passive_crowd.train(x, y, skip_zeros=True)
 predictions = passive_crowd.predict(a, b, x)
-plt.subplot(2, 3, 1)
-plt.title('Groundtruth')
-plt.scatter(x[z.ravel() == 0, 0], x[z.ravel() == 0, 1], c='blue', marker='x')
-plt.scatter(x[z.ravel() == 1, 0], x[z.ravel() == 1, 1], c='red', marker='x')
+
+## Plots ##
+
+fig = plt.figure()
+
+# Plot the groundtruth.
+ax = fig.add_subplot(2, 3, 1)
+ax.set_title('Groundtruth')
+ax.scatter(x[z.ravel() == 0, 0], x[z.ravel() == 0, 1], c='blue', marker='x',
+           s=20)
+ax.scatter(x[z.ravel() == 1, 0], x[z.ravel() == 1, 1], c='red', marker='x',
+           s=20)
+
+# Plot each annotator.
+# zz is used to plot the regions of expertise.
+xx, yy = numpy.meshgrid(numpy.linspace(x[:, 0].min(), x[:, 0].max()),
+                        numpy.linspace(x[:, 1].min(), x[:, 1].max()))
+zz = k_means.predict(numpy.vstack([xx.ravel(), yy.ravel()]).T)
+zz = zz.reshape(xx.shape)
 for t in range(4):
-    if t >= 2:
-        plt.subplot(2, 3, 2 + t + 1)
-    else:
-        plt.subplot(2, 3, 2 + t)
-    plt.title('Annotator {}'.format(t + 1))
-    plt.scatter(x[y[t] == 0, 0], x[y[t] == 0, 1], c='blue', marker='x')
-    plt.scatter(x[y[t] == 1, 0], x[y[t] == 1, 1], c='red', marker='x')
-plt.subplot(2, 3, 4)
-plt.title('Predictions')
-plt.scatter(x[predictions.ravel() == 0, 0], x[predictions.ravel() == 0, 1], c='blue', marker='x')
-plt.scatter(x[predictions.ravel() == 1, 0], x[predictions.ravel() == 1, 1], c='red', marker='x')
+    ax = fig.add_subplot(2, 3, 2 + t)
+    ax.set_title('Annotator {}'.format(t + 1))
+
+    # Plot the region of expertise.
+    ax.contourf(xx, yy, zz == t, cmap='Paired', alpha=0.4)
+    ax.scatter(x[y[t] == 0, 0], x[y[t] == 0, 1], c='blue', marker='x', s=20)
+    ax.scatter(x[y[t] == 1, 0], x[y[t] == 1, 1], c='red', marker='x', s=20)
+
+# Plot predictions.
+ax = fig.add_subplot(2, 3, 6)
+ax.set_title('Predictions')
+ax.scatter(x[predictions.ravel() == 0, 0], x[predictions.ravel() == 0, 1],
+           c='blue', marker='x', s=20)
+ax.scatter(x[predictions.ravel() == 1, 0], x[predictions.ravel() == 1, 1],
+           c='red', marker='x', s=20)
+
 plt.show()
