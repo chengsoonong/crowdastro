95e4bcbbea22c3334d056ae28ddcf72fe0c34a71
MatthewJA <matthew.alger@gmail.com>
2016-09-01 13:30:19 +1000

    Incremented version

--- crowdastro/__init__.py
+++ crowdastro/__init__.py
@@ -7,7 +7,7 @@ The Australian National University
 2016
 """
 
-__version__ = '0.0.1a6'
+__version__ = '0.0.1a7'
 __description__ = 'Automated cross-identification of radio objects and host ' \
                   'galaxies using crowdsourced labels from the Radio Galaxy ' \
                   'Zoo.'
