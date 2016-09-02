91f06a2050add345a50e0b54aa73962657292224
MatthewJA <matthew.alger@gmail.com>
2016-09-02 12:42:31 +1000

    Added LR/RF experiment #156

---
+++ crowdastro/experiment/experiment_lr_rf.py
@@ -0,0 +1,82 @@
+"""Compares logistic regression to random forests, trained on Norris and Fan.
+
+Matthew Alger
+The Australian National University
+2016
+"""
+
+import argparse
+import collections
+import logging
+
+import h5py
+import matplotlib.pyplot as plt
+import numpy
+import sklearn
+
+from . import runners
+from .results import Results
+from ..plot import vertical_scatter_ba
+
+
+def main(crowdastro_h5_path, training_h5_path, results_h5_path,
+         overwrite=False, plot=False):
+    with h5py.File(crowdastro_h5_path, 'r') as crowdastro_h5, \
+            h5py.File(training_h5_path, 'r') as training_h5:
+
+        n_splits = crowdastro_h5['/wise/cdfs/test_sets'].shape[0]
+        n_examples, n_params = training_h5['features'].shape
+        n_params += 1  # Bias term.
+        methods = ['LR(Norris)', 'LR(Fan)', 'RF(Norris)', 'RF(Fan)']
+        model = '{} sklearn.linear_model.LogisticRegression'.format(
+                sklearn.__version__)  # No model for RF.
+
+        results = Results(results_h5_path, methods, n_splits, n_examples,
+                          n_params, model)
+
+        features = collections.defaultdict(
+                lambda: training_h5['features'].value)
+        targets = {
+            'LR(Norris)': crowdastro_h5['/wise/cdfs/norris_labels'],
+            'LR(Fan)': crowdastro_h5['/wise/cdfs/fan_labels'],
+            'RF(Norris)': crowdastro_h5['/wise/cdfs/norris_labels'],
+            'RF(Fan)': crowdastro_h5['/wise/cdfs/fan_labels'],
+        }
+
+        for split_id, test_set in enumerate(
+                    crowdastro_h5['/wise/cdfs/test_sets']):
+            logging.info('Test {}/{}'.format(split_id + 1, n_splits))
+            for method_id, method in enumerate(methods):
+                if method.startswith('LR'):
+                    runner = runners.lr
+                else:
+                    runner = runners.rf
+
+                logging.info('Method {} ({}/{})'.format(method, method_id + 1,
+                                                        len(methods)))
+                runner(results, method, split_id, features[method],
+                       targets[method], list(test_set), overwrite=overwrite)
+
+        if plot:
+            vertical_scatter_ba(results,
+                    crowdastro_h5['/wise/cdfs/norris_labels'].value)
+            plt.show()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--crowdastro', default='data/crowdastro.h5',
+                        help='HDF5 crowdastro data file')
+    parser.add_argument('--training', default='data/training.h5',
+                        help='HDF5 training data file')
+    parser.add_argument('--results', default='data/results_lr_rf.h5',
+                        help='HDF5 results data file')
+    parser.add_argument('--overwrite', action='store_true',
+                        help='Overwrite existing results')
+    parser.add_argument('--plot', action='store_true', help='Generate a plot')
+    args = parser.parse_args()
+
+    logging.root.setLevel(logging.INFO)
+
+    main(args.crowdastro, args.training, args.results, overwrite=args.overwrite,
+         plot=args.plot)
--- crowdastro/experiment/runners.py
+++ crowdastro/experiment/runners.py
@@ -8,12 +8,23 @@ The Australian National University
 import logging
 
 import numpy
+import sklearn.ensemble
 import sklearn.linear_model
 
 
-def to_params(lr):
+def lr_to_params(lr):
     """Converts a LogisticRegression object into an array."""
     return numpy.concatenate([lr.coef_.ravel(), lr.intercept_])
+
+
+def make_train_indices(test_indices, n_examples):
+    """Makes a set of train indices from a set of test indices.
+
+    test_indices: List of test indices.
+    n_examples: Number of total (testing + training) examples.
+    -> sorted list of training indices.
+    """
+    return sorted(set(range(n_examples)) - set(test_indices))
 
 
 def lr(results, method_name, split_id, features, targets, test_indices, C=1.0,
@@ -33,7 +44,7 @@ def lr(results, method_name, split_id, features, targets, test_indices, C=1.0,
     assert max(test_indices) < features.shape[0]
     assert min(test_indices) >= 0
 
-    train_indices = sorted(set(range(features.shape[0])) - set(test_indices))
+    train_indices = make_train_indices(test_indices, features.shape[0])
 
     if results.has_run(method_name, split_id) and not overwrite:
         logging.info('Skipping trial {}:{}.'.format(method_name, split_id))
@@ -43,4 +54,34 @@ def lr(results, method_name, split_id, features, targets, test_indices, C=1.0,
     lr.fit(features[train_indices], targets[train_indices])
     results.store_trial(method_name, split_id,
             lr.predict_proba(features[test_indices])[:, 1],
-            indices=test_indices, params=to_params(lr))
+            indices=test_indices, params=lr_to_params(lr))
+
+
+def rf(results, method_name, split_id, features, targets, test_indices,
+       overwrite=False):
+    """Run random forest and store results.
+
+    Does not store the model as random forests are difficult to serialise.
+
+    results: Results object.
+    features: (n_examples, n_features) array of features.
+    targets: (n_examples,) array of binary targets.
+    test_indices: List of integer testing indices.
+    method_name: Name of this method in the results.
+    split_id: ID of the split in the results.
+    overwrite: Whether to overwrite existing results (default False).
+    """
+    assert max(test_indices) < features.shape[0]
+    assert min(test_indices) >= 0
+
+    train_indices = make_train_indices(test_indices, features.shape[0])
+
+    if results.has_run(method_name, split_id) and not overwrite:
+        logging.info('Skipping trial {}:{}.'.format(method_name, split_id))
+        return
+
+    rf = sklearn.ensemble.RandomForestClassifier(class_weight='balanced')
+    rf.fit(features[train_indices], targets[train_indices])
+    results.store_trial(method_name, split_id,
+            rf.predict_proba(features[test_indices])[:, 1],
+            indices=test_indices, params=numpy.zeros((results.n_params,)))
