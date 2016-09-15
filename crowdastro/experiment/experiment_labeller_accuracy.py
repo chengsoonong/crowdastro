6e4e3b2c3066a8a4edafaae29ff3bf6521dc5c52
MatthewJA <matthew.alger@gmail.com>
2016-09-15 15:30:16 +1000

    Added annotator quality plots. Resolves #178.

--- crowdastro/experiment/experiment_labeller_accuracy.py
+++ crowdastro/experiment/experiment_labeller_accuracy.py
@@ -9,8 +9,10 @@ import argparse
 
 import crowdastro.crowd.util
 import h5py
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy
+import sklearn.metrics
 
 
 def main(crowdastro_h5_path):
@@ -22,17 +24,41 @@ def main(crowdastro_h5_path):
         norris_labels = f_h5['/{}/cdfs/norris_labels'.format(ir_survey)].value
 
         accuracies = []
+        alphas = []  # True positive rate.
+        betas = []  # 1 - false positive rate.
+
         for t in range(labels.shape[0]):
+            mask = ~masked_labels[t].mask
+            cm = sklearn.metrics.confusion_matrix(
+                    norris_labels[mask], masked_labels[t][mask])
             ba = crowdastro.crowd.util.balanced_accuracy(
                     norris_labels, masked_labels[t])
             if ba:
+                alphas.append(cm[1, 1] / (cm[1, 1] + cm[1, 0]))
+                betas.append(1 - cm[0, 1] / (cm[0, 1] + cm[0, 0]))
                 accuracies.append(ba)
 
         print('Average accuracy: ({} +- {})%'.format(
                 numpy.mean(accuracies), numpy.std(accuracies)))
+        print('Average alpha: ({} +- {})%'.format(
+                numpy.mean(alphas), numpy.std(alphas)))
+        print('Average beta: ({} +- {})%'.format(
+                numpy.mean(betas), numpy.std(betas)))
 
-        plt.hist(accuracies, bins=20)
+        matplotlib.rcParams['font.family'] = 'serif'
+        matplotlib.rcParams['font.serif'] = ['Palatino Linotype']
+
+        plt.hist(accuracies, bins=20, color='grey')
         plt.xlabel('Balanced accuracy')
+        plt.ylabel('Number of labellers')
+        plt.show()
+
+        plt.hist(alphas, bins=20, color='grey')
+        plt.xlabel('True positive rate')
+        plt.ylabel('Number of labellers')
+        plt.show()
+        plt.hist(betas, bins=20, color='grey')
+        plt.xlabel('1 - false positive rate')
         plt.ylabel('Number of labellers')
         plt.show()
 
