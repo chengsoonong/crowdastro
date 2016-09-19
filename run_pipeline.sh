946d176720d4c77d613264ef60fd5bb0a524a615
Cheng Soon Ong <chengsoon.ong@anu.edu.au>
2016-09-20 09:00:30 +1000

    Updated shell pipeline runner

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,9 +1,8 @@
-python3 -m crowdastro.import_data
-python3 -m crowdastro.consensuses
-python3 -m crowdastro.generate_training_data
-python3 -m crowdastro.generate_image_patches
-python3 -m crowdastro.compile_cnn
-python3 -m crowdastro.train_cnn
-python3 -m crowdastro.generate_cnn_outputs
-python3 -m crowdastro.train_lr
-python3 -m crowdastro.test_lr
+crowdastro import_data
+crowdastro consensuses
+crowdastro generate_training_data
+crowdastro compile_cnn
+crowdastro train_cnn
+crowdastro generate_cnn_outputs
+crowdastro train --classifier lr
+crowdastro test
