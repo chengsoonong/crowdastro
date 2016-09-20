bbdbb7bb1041f04a1d80ad2de69c30c06dccc5de
MatthewJA <matthew.alger@gmail.com>
2016-09-20 15:07:46 +1000

    Style fixes for classifier

--- crowdastro/classifier.py
+++ crowdastro/classifier.py
@@ -14,15 +14,17 @@ import sklearn.linear_model
 import sklearn.pipeline
 import sklearn.preprocessing
 
-from .config import config
+from crowdastro.config import config
 
 ARCMIN = 1 / 60  # deg
-IMAGE_SIZE = (config['surveys']['atlas']['fits_height']
-             * config['surveys']['atlas']['fits_width'])
+IMAGE_SIZE = (
+    config['surveys']['atlas']['fits_height'] *
+    config['surveys']['atlas']['fits_width'])
 
 
 class RGZClassifier(object):
-    
+    """Locates the host galaxy for the radio object localisation task."""
+
     def __init__(self, ir_features, n_astro,
                  Classifier=sklearn.linear_model.LogisticRegression):
         self.ir_features = ir_features
@@ -35,27 +37,27 @@ class RGZClassifier(object):
         self._image_transformer = sklearn.pipeline.Pipeline([
             ('normalise', sklearn.preprocessing.Normalizer()),
         ])
-    
+
     def _fit_transformers(self, ir_indices):
         self._astro_transformer.fit(self.ir_features[ir_indices, :self.n_astro])
         self._image_transformer.fit(self.ir_features[ir_indices, self.n_astro:])
-    
+
     def _transform(self, features):
         return numpy.hstack([
             self._astro_transformer.transform(features[:, :self.n_astro]),
             self._image_transformer.transform(features[:, self.n_astro:]),
         ])
-    
+
     def train(self, ir_indices, ir_labels):
         ir_indices = list(ir_indices)
         self._fit_transformers(ir_indices)
         ir_features = self._transform(self.ir_features[ir_indices])
         self._classifier.fit(ir_features, ir_labels)
-    
+
     def _predict(self, atlas_vector):
         # Split the ATLAS vector into its components.
-        position = atlas_vector[:2]
-        image = atlas_vector[2:2 + IMAGE_SIZE]
+        # position = atlas_vector[:2]
+        # image = atlas_vector[2:2 + IMAGE_SIZE]
         distances = atlas_vector[2 + IMAGE_SIZE:]
         # Get nearby IR objects and their features.
         nearby_indices = list((distances < ARCMIN).nonzero()[0])
@@ -63,12 +65,12 @@ class RGZClassifier(object):
         # Find how likely each object is to be the host galaxy.
         probabilities = self._classifier.predict_proba(ir_features)[:, 1]
         return nearby_indices, probabilities
-    
+
     def predict(self, atlas_vector):
         nearby_indices, probabilities = self._predict(atlas_vector)
         # Return the index of the most likely host galaxy.
         return nearby_indices[probabilities.argmax()]
-    
+
     def predict_probabilities(self, atlas_vector):
         nearby_indices, probabilities = self._predict(atlas_vector)
         out_probabilities = numpy.zeros(self.ir_features.shape[0])
@@ -83,13 +85,19 @@ def generate_subset(p, indices):
 
 
 class RGZCommittee(object):
+    """Committee of RGZClassifiers."""
 
     def __init__(self, n_committee, ir_features, n_astro, lr_frac):
-        self.classifiers = [RGZClassifier(ir_features, n_astro)
-                            for _ in range(round(lr_frac * n_committee))]
-        self.classifiers += [RGZClassifier(ir_features, n_astro,
-                                           Classifier=RandomForestClassifier)
-                            for _ in range(round((1 - lr_frac) * n_committee))]
+        # Logistic regression.
+        self.classifiers = [
+            RGZClassifier(ir_features, n_astro)
+            for _ in range(round(lr_frac * n_committee))]
+        # Random forest.
+        self.classifiers += [
+            RGZClassifier(
+                ir_features, n_astro, Classifier=RandomForestClassifier)
+            for _ in range(round((1 - lr_frac) * n_committee))]
+
         assert len(self.classifiers) == n_committee
 
     def train(self, ir_indices, ir_labels):
@@ -129,12 +137,12 @@ class RGZCommittee(object):
                     nearby_indices = (distances < ARCMIN).nonzero()[0]
                     for ir_index in nearby_indices:
                         train_ir_indices.add(ir_index)
-                
+
                 train_ir_indices -= test_ir_indices
                 train_ir_indices = numpy.array(sorted(train_ir_indices))
 
                 classifier.train(train_ir_indices, ir_labels[train_ir_indices])
-                
+
                 # Classify all the test subjects.
                 for atlas_index in test_indices:
                     classification = classifier.predict(
