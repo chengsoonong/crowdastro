4cf0ef439d74713fe8ba19ccab234d35014b6b73
MatthewJA <matthew.alger@gmail.com>
2016-09-20 15:03:02 +1000

    Style fixes for __main__

--- crowdastro/__main__.py
+++ crowdastro/__main__.py
@@ -27,60 +27,62 @@ from crowdastro import train_cnn
 
 def main():
     parser = argparse.ArgumentParser(description=__description__)
-    parser.add_argument('--verbose', '--v', '-v', action='store_true',
-            help='verbose output')
-    parser.add_argument('--version', action='store_true',
-            help='get version number')
+    parser.add_argument(
+        '--verbose', '--v', '-v', action='store_true', help='verbose output')
+    parser.add_argument(
+        '--version', action='store_true', help='get version number')
     subparsers = parser.add_subparsers(dest='subcommand')
 
-    parser_compile_cnn = subparsers.add_parser('compile_cnn',
-            help='compile a convolutional neural network')
+    parser_compile_cnn = subparsers.add_parser(
+        'compile_cnn', help='compile a convolutional neural network')
     compile_cnn._populate_parser(parser_compile_cnn)
 
-    parser_consensuses = subparsers.add_parser('consensuses',
-            help='generate Radio Galaxy Zoo consensus classifications')
+    parser_consensuses = subparsers.add_parser(
+        'consensuses',
+        help='generate Radio Galaxy Zoo consensus classifications')
     consensuses._populate_parser(parser_consensuses)
 
     parser_generate_annotator_labels = subparsers.add_parser(
-            'generate_annotator_labels',
-            help='generates individual annotator labels')
+        'generate_annotator_labels',
+        help='generates individual annotator labels')
     generate_annotator_labels._populate_parser(parser_generate_annotator_labels)
 
-    parser_generate_cnn_outputs = subparsers.add_parser('generate_cnn_outputs',
-            help='generate convolutional neural network training outputs')
+    parser_generate_cnn_outputs = subparsers.add_parser(
+        'generate_cnn_outputs',
+        help='generate convolutional neural network training outputs')
     generate_cnn_outputs._populate_parser(parser_generate_cnn_outputs)
 
-    parser_generate_dataset = subparsers.add_parser('generate_dataset',
-            help='generate crowdastro dataset')
+    parser_generate_dataset = subparsers.add_parser(
+        'generate_dataset', help='generate crowdastro dataset')
     generate_dataset._populate_parser(parser_generate_dataset)
 
-    parser_generate_test_sets = subparsers.add_parser('generate_test_sets',
-            help='generate crowdastro galaxy test sets')
+    parser_generate_test_sets = subparsers.add_parser(
+        'generate_test_sets', help='generate crowdastro galaxy test sets')
     generate_test_sets._populate_parser(parser_generate_test_sets)
 
     parser_generate_training_data = subparsers.add_parser(
-            'generate_training_data',
-            help='generate crowdastro galaxy training data')
+        'generate_training_data',
+        help='generate crowdastro galaxy training data')
     generate_training_data._populate_parser(parser_generate_training_data)
 
-    parser_import_data = subparsers.add_parser('import_data',
-            help='import data into crowdastro')
+    parser_import_data = subparsers.add_parser(
+        'import_data', help='import data into crowdastro')
     import_data._populate_parser(parser_import_data)
 
-    parser_repack_h5 = subparsers.add_parser('repack_h5',
-            help='repacks an HDF5 file')
+    parser_repack_h5 = subparsers.add_parser(
+        'repack_h5', help='repacks an HDF5 file')
     repack_h5._populate_parser(parser_repack_h5)
 
-    parser_test = subparsers.add_parser('test',
-            help='tests classifiers')
+    parser_test = subparsers.add_parser(
+        'test', help='tests classifiers')
     test._populate_parser(parser_test)
 
-    parser_train = subparsers.add_parser('train',
-            help='trains classifiers')
+    parser_train = subparsers.add_parser(
+        'train', help='trains classifiers')
     train._populate_parser(parser_train)
 
-    parser_train_cnn = subparsers.add_parser('train_cnn',
-            help='trains a convolutional neural network')
+    parser_train_cnn = subparsers.add_parser(
+        'train_cnn', help='trains a convolutional neural network')
     train_cnn._populate_parser(parser_train_cnn)
 
     # http://stackoverflow.com/a/11287731/1105803
