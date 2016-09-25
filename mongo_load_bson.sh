89f1de284333898d6eb042cd67e1e3c17999696f
MatthewJA <matthew.alger@gmail.com>
2016-09-25 12:31:59 +1000

    Updated data sources to use the newer dataset, #183

---
+++ mongo_load_bson.sh
@@ -0,0 +1,3 @@
+mongorestore --db radio --collection radio_classifications data/radio_classifications.bson --drop
+mongorestore --db radio --collection radio_groups data/radio_groups.bson --drop
+mongorestore --db radio --collection radio_subjects data/radio_subjects.bson --drop
--- setup_data.sh
+++ setup_data.sh
@@ -1,7 +1,7 @@
 mkdir data
-tar -zxvf sanitized_radio_2016-03-01.tar.gz -C data
-mv data/sanitized_radio_2016-03-01/* data/
-rmdir data/sanitized_radio_2016-03-01
+tar -zxvf radio_galaxy_zoo_2016-08-28.tar.gz -C data
+mv data/radio_galaxy_zoo_2016-08-28/* data/
+rmdir data/radio_galaxy_zoo_2016-08-28
 gunzip -c ATLASDR3_cmpcat_23July2015.dat.gz > data/ATLASDR3_cmpcat_23July2015.dat
 gunzip -c SWIRE3_CDFS_cat_IRAC24_21Dec05.tbl.gz > data/SWIRE3_CDFS_cat_IRAC24_21Dec05.tbl
 mkdir data/elais
