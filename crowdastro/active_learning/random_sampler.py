921752f754cc42a81022e3686099e506395536d1
MatthewJA <matthew.alger@gmail.com>
2016-10-19 15:24:37 +1100

    Updated random sampler

--- crowdastro/active_learning/random_sampler.py
+++ crowdastro/active_learning/random_sampler.py
@@ -38,3 +38,93 @@ class RandomSampler(Sampler):
             indices.add(index)
 
         return sorted(indices)
+
+
+class StratifiedSampler(RandomSampler):
+    """Pool-based learning with stratified random sampling.
+
+    WARNING: This class can "peek" at the true labels!
+    """
+
+    def sample_index(self):
+        """Finds index of a random unlabelled point."""
+        unlabelled = self.labels.mask.nonzero()[0]
+        unlabelled_groundtruth = self.labels.data[unlabelled]
+
+        if len(unlabelled):
+            positive_pc = unlabelled_groundtruth.mean()
+            if numpy.random.random() < positive_pc:
+                index = numpy.random.choice(
+                    unlabelled[unlabelled_groundtruth == 1])
+            else:
+                index = numpy.random.choice(
+                    unlabelled[unlabelled_groundtruth == 0])
+            return index
+
+        return 0
+
+    def sample_indices(self, n):
+        """Finds indices of n random unlabelled points."""
+        indices = set()
+        unlabelled = self.labels.mask.nonzero()[0]
+
+        if len(unlabelled) < n:
+            return unlabelled
+
+        unlabelled_groundtruth = self.labels.data[unlabelled]
+        positive_pc = unlabelled_groundtruth.mean()
+        while len(indices) < n:
+            if numpy.random.random() < positive_pc:
+                index = numpy.random.choice(
+                    unlabelled[unlabelled_groundtruth == 1])
+            else:
+                index = numpy.random.choice(
+                    unlabelled[unlabelled_groundtruth == 0])
+            indices.add(index)
+
+        return sorted(indices)
+
+
+class BalancedSampler(RandomSampler):
+    """Pool-based learning with balanced random sampling.
+
+    WARNING: This class can "peek" at the true labels!
+    """
+
+    def sample_index(self):
+        """Finds index of a random unlabelled point."""
+        unlabelled = self.labels.mask.nonzero()[0]
+        unlabelled_groundtruth = self.labels.data[unlabelled]
+
+        if len(unlabelled):
+            if numpy.random.random() < 0.5:
+                index = numpy.random.choice(
+                    unlabelled[unlabelled_groundtruth == 1])
+            else:
+                index = numpy.random.choice(
+                    unlabelled[unlabelled_groundtruth == 0])
+            return index
+
+        return 0
+
+    def sample_indices(self, n):
+        """Finds indices of n random unlabelled points."""
+        indices = set()
+        unlabelled = self.labels.mask.nonzero()[0]
+
+        if len(unlabelled) < n:
+            return unlabelled
+
+        unlabelled_groundtruth = self.labels.data[unlabelled]
+        while len(indices) < n:
+            if ((numpy.random.random() < 0.5 and
+                    len(unlabelled[unlabelled_groundtruth == 1]) > 0) or
+                    len(unlabelled[unlabelled_groundtruth == 0]) == 0):
+                index = numpy.random.choice(
+                    unlabelled[unlabelled_groundtruth == 1])
+            else:
+                index = numpy.random.choice(
+                    unlabelled[unlabelled_groundtruth == 0])
+            indices.add(index)
+
+        return sorted(indices)
